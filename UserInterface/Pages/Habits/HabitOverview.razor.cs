9db503e231676f75db704ab0006230248ad737a3
Ryan Heitmann <ryan.heitmann@gmail.com>
2022-06-04 15:51:25 +1200

    Scaffolding up Habit UI

--- UserInterface/Pages/Habits/HabitOverview.razor.cs
+++ UserInterface/Pages/Habits/HabitOverview.razor.cs
@@ -6,26 +6,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UserInterface.Services.Interfaces;
+using UserInterface.ViewModels;
 
 namespace UserInterface.Pages.Habits
 {
     public partial class HabitOverview
     {
-        public IEnumerable<Habit> Habits { get; set; }
+        public IEnumerable<HabitViewModel> HabitViewModels { get; set; }
         [Inject]
         public IHabitDataService HabitDataService { get; set; }
         public List<HabitRoutine> ViewableRoutines { get; set; }
-        public bool showRoutine { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Habits = (await HabitDataService.GetAllHabits()).ToList();
+            var userHabits = (await HabitDataService.GetAllHabits()).ToList();
+            HabitViewModels = AssignHabitViewModels(userHabits);
         }
 
         private void ShowBtnPress(int habitId)
         {
-            ViewableRoutines = Habits.FirstOrDefault(h => h.Id == habitId).HabitRoutines.ToList();
-            showRoutine = !showRoutine;
+            var selectedHabit = HabitViewModels.FirstOrDefault(h => h.Id == habitId);
+            selectedHabit.isVisible = !selectedHabit.isVisible;
+        }
+
+        private HabitViewModel ghettoMapper(Habit habit)
+        {
+            var newHabitVM = new HabitViewModel();
+            newHabitVM.Id = habit.Id;
+            newHabitVM.Title = habit.Title;
+            newHabitVM.Description = habit.Description;
+            newHabitVM.HabitRoutines = habit.HabitRoutines;
+            newHabitVM.isVisible = false;
+            return newHabitVM;
+        }
+        private IEnumerable<HabitViewModel> AssignHabitViewModels(List<Habit> habits)
+        {
+            var habitVMs = new List<HabitViewModel>();
+            foreach (var habit in habits)
+            {
+                habitVMs.Add(ghettoMapper(habit));
+            }
+            return habitVMs;
         }
     }
 }
